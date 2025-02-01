65433517d71b098da193b72ebc59871acb531005
Naziur Rahman Khan (Aiman) <115534043+naziurrahmankhan101@users.noreply.github.com>
2025-02-01 12:13:56 +0600

    home page layout updated

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,8 +1,8 @@
 <header class="flex items-center justify-between py-3 px-6 border-b border-gray-100">
     <div id="header-left" class="flex items-center">
-        <div class="text-gray-800 font-semibold">
-            <span class="text-yellow-500 text-xl">&lt;YELO&gt;</span> Code
-        </div>
+        <a href="{{ route('home') }}">
+            <x-application-logo />
+        </a>
         <div class="top-menu ml-10">
             <ul class="flex space-x-4">
                 <li>
@@ -11,30 +11,35 @@
                         Home
                     </a>
                 </li>
+
                 <li>
                     <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
                         href="http://127.0.0.1:8000/blog">
                         Blog
                     </a>
                 </li>
+
                 <li>
                     <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
                         href="http://127.0.0.1:8000/blog">
                         About Us
                     </a>
                 </li>
+
                 <li>
                     <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
                         href="http://127.0.0.1:8000/blog">
                         Contact Us
                     </a>
                 </li>
+
                 <li>
                     <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
                         href="http://127.0.0.1:8000/blog">
                         Terms
                     </a>
                 </li>
+
             </ul>
         </div>
     </div>
@@ -42,6 +47,7 @@
         @guest
             @include('layouts.partials.header-right-guest')
         @endguest
+
         @auth()
             @include('layouts.partials.header-right-auth')
         @endauth
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -8,19 +9,17 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class)->name('home');
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
-    'verified'
+    'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
