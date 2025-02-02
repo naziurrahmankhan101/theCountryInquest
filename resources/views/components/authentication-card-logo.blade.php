b42e08b7fd5fd302a5ebe70d656404959c646d59
m n <marjanasalauddin@gmail.com>
2025-02-02 15:12:32 +0600

    test commit

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,5 +1,5 @@
 <a href="/">
     <div class="text-gray-800 font-semibold">
-        <span class="text-yellow-500 text-3xl">&lt;The Country Inquest&gt;</span> Code
+        <span class="text-yellow-500 text-3xl">&lt;The Country Inquest&gt;</span> News
     </div>
 </a>
